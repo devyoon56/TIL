198dcf9d9c7819c6a7d03df1fe8689de8b5f8065
devyoon56 <libdev56@gmail.com>
2022-10-29 00:01:19 +0900

    폴더 생성 및 간단 예제 확인

---
+++ Book/누구나 자료 구조와 알고리즘/print_numbers.py
@@ -0,0 +1,14 @@
+def print_numbers_version_one():
+  number = 2
+
+  while number <= 100:
+    if number % 2 == 0:
+      print(number)
+    number += 1
+
+def print_numbers_version_two():
+  number = 2
+
+  while number <= 100:
+    print(number)
+    number += 2
